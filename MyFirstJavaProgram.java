63070410f6f979386cc1a22978ea300513a74cfa
fencim <fencim@gmail.com>
2022-05-17 03:46:33 +0800

    working with arrays

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
 public class MyFirstJavaProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int x = 0;
-        while (x < 10) {
-            System.out.print(x);
-            System.out.print(" ");
-            x++;
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+        scores.add(12);
+        scores.add(0);
+        scores.add(5);
+        System.out.println(scores.size());
+        for (int i = 0; i < scores.size(); i++) {
+            System.out.println(scores.get(i));
+        }
+
+        Object[] arrayScores = scores.toArray();
+        System.out.println(arrayScores.length);
+        for (int i = 0; i < arrayScores.length; i++) {
+            System.out.println(arrayScores[i]);
         }
     }
 }
