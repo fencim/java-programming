59c77bc763930161e35c2e336995aa547ca83f29
fencim <fencim@gmail.com>
2022-06-18 17:35:57 +0800

    dynamic input

--- ArrayFromUser.java
+++ ArrayFromUser.java
@@ -4,16 +4,12 @@ public class ArrayFromUser {
     public static void main(String[] args) {
         ArrayList<Integer> scores = new ArrayList<Integer>();
         Scanner userInput = new Scanner(System.in);
-        System.out.println("How many scores do you want to enter");
-        int scoreCount = userInput.nextInt();
-
-        System.out.println("You need to enter "+ scoreCount + " scores");
-        int counter = 0;
-        while (counter < scoreCount) {
-            //ask ng input
+        
+        System.out.println("Please enter scores:");
+        
+        while (userInput.hasNextInt()) {
             int score = userInput.nextInt();
             scores.add(score);
-            counter++;
         }
 
         for (int i = 0; i < scores.size(); i++) {
