f87bc33f1cf401a3b27a8acc63cc81ea8ea63b0a
fencim <fencim@gmail.com>
2022-06-18 15:13:39 +0800

    get score count from user

--- ArrayFromUser.java
+++ ArrayFromUser.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class ArrayFromUser {
     public static void main(String[] args) {
-        System.out.println("This program is all about user input");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("How many scores do you want to enter");
+        int scoreCount = userInput.nextInt();
+
+        System.out.println("You need to enter "+ scoreCount + " scores");
+
+        userInput.close();
     }
 }
