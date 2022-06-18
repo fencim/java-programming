6c28fddaf8b5012d6f1bbad1baf0d6481295de7c
fencim <fencim@gmail.com>
2022-06-19 00:53:01 +0800

    implement bubble sort

--- SortingArray.java
+++ SortingArray.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class SortingArray {
+    static ArrayList<Integer> scores = new ArrayList<Integer>();
     public static void main(String[] args) {
-        ArrayList<Integer> scores = new ArrayList<Integer>();
+        
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("Please enter the scores");
@@ -10,11 +11,29 @@ public class SortingArray {
             int score = userInput.nextInt();
             scores.add(score);
         }
+        printItems();
+        System.out.println("NOw sorting with bubble sort");
+        bubbleSort();
+        printItems();
+        
+        userInput.close();
+    }
+    static void bubbleSort() {
+        for (int pass = 1; pass <= scores.size() - 1; pass++) {
+            for (int i = 0; i < scores.size() - 1; i++) {
+                if (scores.get(i) < scores.get(i + 1)) {
+                    //swapping
+                    int temp = scores.get(i);
+                    scores.set(i, scores.get(i + 1));
+                    scores.set(i + 1, temp);
+                }
+            }
+        }
+    }
+    static void printItems() {
         for (int i = 0; i < scores.size(); i++) {
             System.out.println("Score at "+ (i + 1) + " is " + scores.get(i));
         }
-        
-        userInput.close();
     }
     
 }
