a36ceb150520df4ddff8b5dc8f77d1f74cb717f3
fencim <fencim@gmail.com>
2022-06-18 15:26:21 +0800

    declare array score

--- ArrayFromUser.java
+++ ArrayFromUser.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class ArrayFromUser {
     public static void main(String[] args) {
+        ArrayList<Integer> scores = new ArrayList<Integer>();
         Scanner userInput = new Scanner(System.in);
         System.out.println("How many scores do you want to enter");
         int scoreCount = userInput.nextInt();
 
         System.out.println("You need to enter "+ scoreCount + " scores");
 
+        
         userInput.close();
     }
 }
